c630993eecdacf3ce2a8b900fabde45f733c4682
bhavilad <bhavi.ld@gmail.com>
2018-04-04 11:40:08 +0400

    V 1.0 - First Commit

---
+++ registration-design/registration-design/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  registration-design
+//
+//  Created by Bhavi Lad on 04/04/18.
+//  Copyright © 2018 Bhavi Lad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
